f132ef1f5b2c9a80f28c26ec247331c2f42dec49
Borca <lorand.borca@siemens.com>
2017-06-05 23:06:05 +0300

    Removed unnecesarry floating buttons in QRPage

--- Client/app/src/main/java/com/example/vlad/licenta/qrGeneratePage.java
+++ Client/app/src/main/java/com/example/vlad/licenta/qrGeneratePage.java
@@ -34,16 +34,6 @@ public class qrGeneratePage extends AppCompatActivity {
                 }
             }
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
     public void setQrImage(Bitmap img){
         ImageView iv = (ImageView) findViewById(R.id.qr_generate_page);
